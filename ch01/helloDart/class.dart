911a42671d843142104080543fd974b8b364d01d
maicoder <hkdjcmc@msn.cn>
2020-04-12 21:42:11 +0800

    Update class.dart

--- ch01/helloDart/class.dart
+++ ch01/helloDart/class.dart
@@ -1,12 +1,17 @@
 main(List<String> args) {
-  final p = Person();
-  p.name = 'dart';
-  p.age = 18;
-  p.eating();
+  // final p = Person();
+  // p.name = 'dart';
+  // p.age = 18;
+  // p.eating();
+
+  final p = Person("flutter", 20);
+  print(p);
 }
 
 class Animal {
   int age; 
+
+  Animal(this.age);
 
   void eating() {
     print('eating...');
@@ -16,8 +21,15 @@ class Animal {
 class Person extends Animal {
   String name;
 
+  Person(this.name, int age): super(age);
+
   @override
   void eating() {
-    print('$name eating...');
+    print("$name eating...");
+  }
+
+  @override
+  String toString() {
+    return "name: $name, age: $age";
   }
 }
