c4b3b8744bb6437c20b13624351681a4791e778b
maicoder <hkdjcmc@msn.cn>
2020-04-12 21:42:43 +0800

    Create abstractClass.dart

---
+++ ch01/helloDart/abstractClass.dart
@@ -0,0 +1,24 @@
+abstract class Shape {
+  getArea();
+}
+
+class Circle extends Shape {
+  double r;
+
+  @override
+  getArea() {
+    return r * r * 3.14;
+  }
+}
+
+class Reactangle extends Shape {
+  double w;
+  double h;
+
+  Reactangle(this.w, this.h);
+
+  @override
+  getArea() {
+    return w * h;
+  }
+}
