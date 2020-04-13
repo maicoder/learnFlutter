02e84c56bd900bfc9baf7bb82d83c6155a7666da
maicoder <hkdjcmc@msn.cn>
2020-04-13 17:44:00 +0800

    Update main.dart

--- ch02/learn_flutter/lib/main.dart
+++ ch02/learn_flutter/lib/main.dart
@@ -4,29 +4,41 @@ import 'package:flutter/material.dart';
 /// 2.执行 runApp 函数
 /// 3.传入 widget -> Text widget
 main(List<String> args) {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("HelloFlutter"),
         ),
-        body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Checkbox(
-                value: true,
-                onChanged: (value) => print("Hello Flutter")
-              ),
-              Text(
-                "同意协议",
-                textDirection: TextDirection.ltr,
-                style: TextStyle(fontSize: 20),
-              )
-            ],
-          ),
-        ),
+        body: HomeContent(),
       ),
-    )
-  );
+    );
+  }
+}
+
+class HomeContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Checkbox(
+            value: true,
+            onChanged: (value) => print("Hello World")
+          ),
+          Text(
+            "同意协议",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 20),
+          )
+        ],
+      ),
+    );
+  }
 }
