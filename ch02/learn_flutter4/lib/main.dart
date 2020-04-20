b3ac6498a6f2375c8a9174373c448b4d91120756
maicoder <hkdjcmc@msn.cn>
2020-04-20 17:03:01 +0800

    Update main.dart

--- ch02/learn_flutter4/lib/main.dart
+++ ch02/learn_flutter4/lib/main.dart
@@ -20,6 +20,32 @@ class MyApp extends StatelessWidget {
 class ContentWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Text.rich(
+      TextSpan(
+        children: [
+          TextSpan(
+              text: "《定风波》",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)
+          ),
+          TextSpan(
+              text: "苏轼",
+              style: TextStyle(fontSize: 18, color: Colors.red)
+          ),
+          TextSpan(
+              text: "\n莫听穿林打叶声，何妨吟啸且徐行。\n竹杖芒鞋轻胜马，谁怕？一蓑烟雨任平生。",
+              style: TextStyle(fontSize: 20, color: Colors.purple)
+          )
+        ]
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+}
+
+class TextDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
     return Text(
       "《定风波》 苏轼 \n莫听穿林打叶声，何妨吟啸且徐行。\n竹杖芒鞋轻胜马，谁怕？一蓑烟雨任平生。",
       style: TextStyle(
