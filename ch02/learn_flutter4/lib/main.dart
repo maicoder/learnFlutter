31839cda1ea646da70a0d931b86a6472cf795333
maicoder <hkdjcmc@msn.cn>
2020-04-20 18:17:02 +0800

    Update main.dart

--- ch02/learn_flutter4/lib/main.dart
+++ ch02/learn_flutter4/lib/main.dart
@@ -20,22 +20,71 @@ class MyApp extends StatelessWidget {
 class ContentWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        onPressed: () => print("按钮发生点击"),
+        color: Colors.red,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Icon(Icons.people, color: Colors.white,),
+            SizedBox(width: 10,),
+            Text("按钮", style: TextStyle(fontSize: 20, color: Colors.white),),
+          ],
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8)
+        ),
+      ),
+    );
+  }
+}
+
+class ButtonDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        RaisedButton(
+          child: Text("RaisedButton Click"),
+          onPressed: () => print("RaisedButton Click"),
+        ),
+        FlatButton(
+            onPressed: () => print("FlatButton Click"),
+            child: Text("FlatButton Click")
+        ),
+        OutlineButton(
+          onPressed: () => print("OutlineButton Click"),
+          child: Text("OutlineButton Click"),
+        ),
+        FloatingActionButton(
+            onPressed: () => print("FloatingActionButton Click"),
+            child: Text("FloatingActionButton Click")
+        )
+      ],
+    );
+  }
+}
+
+class TextRichDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Text.rich(
       TextSpan(
-        children: [
-          TextSpan(
-              text: "《定风波》",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)
-          ),
-          TextSpan(
-              text: "苏轼",
-              style: TextStyle(fontSize: 18, color: Colors.red)
-          ),
-          TextSpan(
-              text: "\n莫听穿林打叶声，何妨吟啸且徐行。\n竹杖芒鞋轻胜马，谁怕？一蓑烟雨任平生。",
-              style: TextStyle(fontSize: 20, color: Colors.purple)
-          )
-        ]
+          children: [
+            TextSpan(
+                text: "《定风波》",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)
+            ),
+            TextSpan(
+                text: "苏轼",
+                style: TextStyle(fontSize: 18, color: Colors.red)
+            ),
+            TextSpan(
+                text: "\n莫听穿林打叶声，何妨吟啸且徐行。\n竹杖芒鞋轻胜马，谁怕？一蓑烟雨任平生。",
+                style: TextStyle(fontSize: 20, color: Colors.purple)
+            )
+          ]
       ),
       textAlign: TextAlign.center,
     );
@@ -45,7 +94,6 @@ class ContentWidget extends StatelessWidget {
 class TextDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Text(
       "《定风波》 苏轼 \n莫听穿林打叶声，何妨吟啸且徐行。\n竹杖芒鞋轻胜马，谁怕？一蓑烟雨任平生。",
       style: TextStyle(
