234bfbc355395fba5563a7ac7691c0ba61cf7fb8
maicoder <hkdjcmc@msn.cn>
2020-04-22 19:24:42 +0800

    Update main.dart

--- ch02/learn_flutter5/lib/main.dart
+++ ch02/learn_flutter5/lib/main.dart
@@ -27,16 +27,35 @@ class MyHomePage extends StatelessWidget {
 class MyHomeBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Icon(Icons.pets, size: 36, color: Colors.red,),
-      widthFactor: 3,
-      heightFactor: 3,
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: Text(
+        "莫听穿林打叶声，何妨吟啸且徐行。竹杖芒鞋轻胜马，谁怕？一蓑烟雨任平生。",
+        style: TextStyle(
+//          backgroundColor: Colors.green,
+//          background: Paint()..color.green,
+          color: Colors.redAccent,
+          fontSize: 18
+        ),
+      ),
     );
   }
 }
 
+
+//class CenterDemo extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Center(
+//      child: Icon(Icons.pets, size: 36, color: Colors.red,),
+//      widthFactor: 3,
+//      heightFactor: 3,
+//    );
+//  }
+//}
+
 /// Align
-//class MyHomeBody extends StatelessWidget {
+//class AlignDemo extends StatelessWidget {
 //  @override
 //  Widget build(BuildContext context) {
 //    return Align(
