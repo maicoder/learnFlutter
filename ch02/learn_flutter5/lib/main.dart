b4f67003eaa7b4b54641c3347875c0b3b3f34e37
maicoder <hkdjcmc@msn.cn>
2020-04-22 19:30:14 +0800

    Update main.dart

--- ch02/learn_flutter5/lib/main.dart
+++ ch02/learn_flutter5/lib/main.dart
@@ -27,20 +27,34 @@ class MyHomePage extends StatelessWidget {
 class MyHomeBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(20),
-      child: Text(
-        "莫听穿林打叶声，何妨吟啸且徐行。竹杖芒鞋轻胜马，谁怕？一蓑烟雨任平生。",
-        style: TextStyle(
-//          backgroundColor: Colors.green,
-//          background: Paint()..color.green,
-          color: Colors.redAccent,
-          fontSize: 18
-        ),
+    return Center(
+      child: Container(
+        color: Color.fromRGBO(3, 3, 255, .5),
+        width: 100,
+        height: 100,
+        child: Icon(Icons.pets, size: 32, color: Colors.white,),
       ),
     );
   }
 }
+
+//class PaddingDemo extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Padding(
+//      padding: EdgeInsets.all(20),
+//      child: Text(
+//        "莫听穿林打叶声，何妨吟啸且徐行。竹杖芒鞋轻胜马，谁怕？一蓑烟雨任平生。",
+//        style: TextStyle(
+////          backgroundColor: Colors.green,
+////          background: Paint()..color.green,
+//          color: Colors.redAccent,
+//          fontSize: 18
+//        ),
+//      ),
+//    );
+//  }
+//}
 
 
 //class CenterDemo extends StatelessWidget {
