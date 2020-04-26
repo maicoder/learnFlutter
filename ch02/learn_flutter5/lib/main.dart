d4780f597be22f52b352443b3e7958d7c7d406b3
maicoder <hkdjcmc@msn.cn>
2020-04-26 09:17:16 +0800

    Update

--- ch02/learn_flutter5/lib/main.dart
+++ ch02/learn_flutter5/lib/main.dart
@@ -30,16 +30,39 @@ class MyHomeBody extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       crossAxisAlignment: CrossAxisAlignment.end,
-//      mainAxisSize: MainAxisSize.max,
+      mainAxisSize: MainAxisSize.max,
       children: <Widget>[
-        Container(color: Colors.red, width: 60, height: 60,),
+        Expanded(
+          flex: 1,
+          child: Container(color: Colors.red, height: 60,),
+        ),
         Container(color: Colors.blue, width: 80, height: 80,),
         Container(color: Colors.green, width: 70, height: 70,),
-        Container(color: Colors.orange, width: 100, height: 100,)
+        Expanded(
+          child: Container(color: Colors.orange, height: 100,),
+          flex: 1,
+        )
       ],
     );
   }
 }
+
+//class RowDemo extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Row(
+//      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//      crossAxisAlignment: CrossAxisAlignment.end,
+////      mainAxisSize: MainAxisSize.max,
+//      children: <Widget>[
+//        Container(color: Colors.red, width: 60, height: 60,),
+//        Container(color: Colors.blue, width: 80, height: 80,),
+//        Container(color: Colors.green, width: 70, height: 70,),
+//        Container(color: Colors.orange, width: 100, height: 100,)
+//      ],
+//    );
+//  }
+//}
 
 //class BorderRadiusDemo extends StatelessWidget {
 //  @override
