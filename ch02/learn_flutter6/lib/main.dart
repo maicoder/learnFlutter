ea4f0927ff92bf36d9f5d33e0215a5b92ca33a19
maicoder <hkdjcmc@msn.cn>
2020-04-27 20:03:20 +0800

    Update main.dart

--- ch02/learn_flutter6/lib/main.dart
+++ ch02/learn_flutter6/lib/main.dart
@@ -7,57 +7,66 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: MyHomePage(),
+      home: MyHomeBody(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => MyHomePageState();
-}
-
-class MyHomePageState extends State<MyHomePage> {
-  ScrollController _controller;
-  bool _isShowTop = false;
-
-  @override
-  void initState() {
-    // 初始化ScrollController
-    _controller = ScrollController();
-
-    // 监听滚动
-    _controller.addListener(() {
-      var tempSsShowTop = _controller.offset >= 1000;
-      if (tempSsShowTop != _isShowTop) {
-        setState(() {
-          _isShowTop = tempSsShowTop;
-        });
-      }
-    });
-
-    super.initState();
-  }
-
+class MyHomeBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("ListView展示"),
-      ),
-      body: ListView.builder(
-          itemCount: 100,
-          itemExtent: 60,
-          controller: _controller,
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(title: Text("item$index"));
-          }
-      ),
-      floatingActionButton: !_isShowTop ? null : FloatingActionButton(
-        child: Icon(Icons.arrow_upward),
-        onPressed: () {
-          _controller.animateTo(0, duration: Duration(milliseconds: 1000), curve: Curves.ease);
-        },
+      appBar: AppBar(title: Text("Hello"),),
+      body: MyHomeNotificationDemo(),
+    );
+  }
+}
+
+class MyHomeNotificationDemo extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MyHomeNotificationDemoState();
+}
+
+class MyHomeNotificationDemoState extends State<MyHomeNotificationDemo> {
+  int _progress = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return NotificationListener(
+      onNotification: (ScrollNotification notification) {
+        // 1.判断监听事件的类型
+        if (notification is ScrollStartNotification) {
+          print("开始滚动.....");
+        } else if (notification is ScrollUpdateNotification) {
+          // 当前滚动的位置和总长度
+          final currentPixel = notification.metrics.pixels;
+          final totalPixel = notification.metrics.maxScrollExtent;
+          double progress = currentPixel / totalPixel;
+          setState(() {
+            _progress = (progress * 100).toInt();
+          });
+          print("正在滚动：${notification.metrics.pixels} - ${notification.metrics.maxScrollExtent}");
+        } else if (notification is ScrollEndNotification) {
+          print("结束滚动....");
+        }
+        return false;
+      },
+      child: Stack(
+        alignment: Alignment(.9, .9),
+        children: <Widget>[
+          ListView.builder(
+              itemCount: 100,
+              itemExtent: 60,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(title: Text("item$index"));
+              }
+          ),
+          CircleAvatar(
+            radius: 30,
+            child: Text("$_progress%"),
+            backgroundColor: Colors.black54,
+          )
+        ],
       ),
     );
   }
