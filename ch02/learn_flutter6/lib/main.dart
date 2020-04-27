05bd266688c62b4c34a72f08d97b44ce7b41dfb5
maicoder <hkdjcmc@msn.cn>
2020-04-27 19:55:49 +0800

    Update main.dart

--- ch02/learn_flutter6/lib/main.dart
+++ ch02/learn_flutter6/lib/main.dart
@@ -12,69 +12,53 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  ScrollController _controller;
+  bool _isShowTop = false;
+
+  @override
+  void initState() {
+    // 初始化ScrollController
+    _controller = ScrollController();
+
+    // 监听滚动
+    _controller.addListener(() {
+      var tempSsShowTop = _controller.offset >= 1000;
+      if (tempSsShowTop != _isShowTop) {
+        setState(() {
+          _isShowTop = tempSsShowTop;
+        });
+      }
+    });
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Hello"),),
-      body: HomeContent(),
-    );
-  }
-}
-
-class HomeContent extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return showCustomScrollView();
-  }
-
-  Widget showCustomScrollView() {
-    return new CustomScrollView(
-      slivers: <Widget>[
-        const SliverAppBar(
-          expandedHeight: 250.0,
-          flexibleSpace: FlexibleSpaceBar(
-            title: Text('Coderwhy Demo'),
-            background: Image(
-              image: NetworkImage(
-                "https://tva1.sinaimg.cn/large/006y8mN6gy1g72j6nk1d4j30u00k0n0j.jpg",
-              ),
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-        new SliverGrid(
-          gridDelegate: new SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200.0,
-            mainAxisSpacing: 10.0,
-            crossAxisSpacing: 10.0,
-            childAspectRatio: 4.0,
-          ),
-          delegate: new SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-              return new Container(
-                alignment: Alignment.center,
-                color: Colors.teal[100 * (index % 9)],
-                child: new Text('grid item $index'),
-              );
-            },
-            childCount: 10,
-          ),
-        ),
-        SliverFixedExtentList(
-          itemExtent: 50.0,
-          delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
-                return new Container(
-                  alignment: Alignment.center,
-                  color: Colors.lightBlue[100 * (index % 9)],
-                  child: new Text('list item $index'),
-                );
-              },
-              childCount: 20
-          ),
-        ),
-      ],
+      appBar: AppBar(
+        title: Text("ListView展示"),
+      ),
+      body: ListView.builder(
+          itemCount: 100,
+          itemExtent: 60,
+          controller: _controller,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(title: Text("item$index"));
+          }
+      ),
+      floatingActionButton: !_isShowTop ? null : FloatingActionButton(
+        child: Icon(Icons.arrow_upward),
+        onPressed: () {
+          _controller.animateTo(0, duration: Duration(milliseconds: 1000), curve: Curves.ease);
+        },
+      ),
     );
   }
 }
