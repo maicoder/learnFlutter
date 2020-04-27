241f3455c5e47de4572ad3300cb1c3d86995af53
maicoder <hkdjcmc@msn.cn>
2020-04-27 16:32:37 +0800

    Update isolate.dart

--- ch01/helloDart/isolate.dart
+++ ch01/helloDart/isolate.dart
@@ -1,9 +1,22 @@
 import "dart:isolate";
 
-main(List<String> args) {
-  Isolate.spawn(foo, "Hello Isolate");
+main(List<String> args) async {
+  // 1.创建管道
+  ReceivePort receivePort= ReceivePort();
+
+  // 2.创建新的Isolate
+  Isolate isolate = await Isolate.spawn<SendPort>(foo, receivePort.sendPort);
+
+  // 3.监听管道消息
+  receivePort.listen((data) {
+    print('Data：$data');
+    // 不再使用时，我们会关闭管道
+    receivePort.close();
+    // 需要将isolate杀死
+    isolate?.kill(priority: Isolate.immediate);
+  });
 }
 
-void foo(info) {
-  print("新的isolate：$info");
+void foo(SendPort sendPort) {
+  sendPort.send("Hello World");
 }
