f15480c3053ab92d0a3de712fe17192390534f7b
maicoder <hkdjcmc@msn.cn>
2020-04-27 19:52:12 +0800

    Update main.dart

--- ch02/learn_flutter6/lib/main.dart
+++ ch02/learn_flutter6/lib/main.dart
@@ -25,30 +25,55 @@ class MyHomePage extends StatelessWidget {
 class HomeContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
+    return showCustomScrollView();
+  }
+
+  Widget showCustomScrollView() {
+    return new CustomScrollView(
       slivers: <Widget>[
-        SliverSafeArea(
-          sliver: SliverPadding(
-            padding: EdgeInsets.all(8),
-            sliver: SliverGrid(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 8,
-                mainAxisSpacing: 8,
+        const SliverAppBar(
+          expandedHeight: 250.0,
+          flexibleSpace: FlexibleSpaceBar(
+            title: Text('Coderwhy Demo'),
+            background: Image(
+              image: NetworkImage(
+                "https://tva1.sinaimg.cn/large/006y8mN6gy1g72j6nk1d4j30u00k0n0j.jpg",
               ),
-              delegate: SliverChildBuilderDelegate(
-                      (BuildContext context, int index) {
-                    return Container(
-                      alignment: Alignment(0, 0),
-                      color: Colors.orange,
-                      child: Text("item$index"),
-                    );
-                  },
-                  childCount: 20
-              ),
+              fit: BoxFit.cover,
             ),
           ),
-        )
+        ),
+        new SliverGrid(
+          gridDelegate: new SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200.0,
+            mainAxisSpacing: 10.0,
+            crossAxisSpacing: 10.0,
+            childAspectRatio: 4.0,
+          ),
+          delegate: new SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+              return new Container(
+                alignment: Alignment.center,
+                color: Colors.teal[100 * (index % 9)],
+                child: new Text('grid item $index'),
+              );
+            },
+            childCount: 10,
+          ),
+        ),
+        SliverFixedExtentList(
+          itemExtent: 50.0,
+          delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                return new Container(
+                  alignment: Alignment.center,
+                  color: Colors.lightBlue[100 * (index % 9)],
+                  child: new Text('list item $index'),
+                );
+              },
+              childCount: 20
+          ),
+        ),
       ],
     );
   }
