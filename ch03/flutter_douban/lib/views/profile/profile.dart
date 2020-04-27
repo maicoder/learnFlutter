9a630cc28beeb69b0a475cec2267e67602428cee
maicoder <hkdjcmc@msn.cn>
2020-04-27 21:54:46 +0800

    Create profile.dart

---
+++ ch03/flutter_douban/lib/views/profile/profile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Profile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text("我的"),),
+      body: Center(
+        child: Text("我的", style: TextStyle(fontSize: 30),),
+      ),
+    );
+  }
+}
