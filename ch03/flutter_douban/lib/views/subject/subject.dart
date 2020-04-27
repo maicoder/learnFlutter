6cd541c874291571bb05067e369a22452004b2bf
maicoder <hkdjcmc@msn.cn>
2020-04-27 21:54:49 +0800

    Create subject.dart

---
+++ ch03/flutter_douban/lib/views/subject/subject.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Subject extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text("书影音"),),
+      body: Center(
+        child: Text("书影音", style: TextStyle(fontSize: 30),),
+      ),
+    );
+  }
+}
